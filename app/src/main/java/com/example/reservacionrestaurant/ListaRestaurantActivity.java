75f0b3f5ed4e2f2831ef923d2036abb16e2eacab
Angel22 <134756758+Angel220718@users.noreply.github.com>
2024-01-10 01:29:20 -0500

    creaccion de scroll para listar restaurant

--- app/src/main/java/com/example/reservacionrestaurant/ListaRestaurantActivity.java
+++ app/src/main/java/com/example/reservacionrestaurant/ListaRestaurantActivity.java
@@ -2,7 +2,6 @@ package com.example.reservacionrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.viewpager.widget.ViewPager;
 
 public class ListaRestaurantActivity extends AppCompatActivity {
 
@@ -10,9 +9,5 @@ public class ListaRestaurantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_restaurant);
-
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        RestaurantPagerAdapter adapter = new RestaurantPagerAdapter(this, getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
     }
 }
